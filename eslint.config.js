982f92387fb4956cf565be6924a121bf646d72b7
rajivsthh <rajivsth0713@gmail.com>
2026-02-15 21:09:11 +0545

    Initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -23,4 +23,10 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
+  {
+    files: ["**/*.css"],
+    rules: {
+      "no-empty": "off",
+    },
+  },
 );
